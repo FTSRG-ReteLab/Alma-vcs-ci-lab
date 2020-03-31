03d07d103821fc4118b757f13344be9828539828
ghymessz <ghymessz@gmail.com>
2020-03-31 16:21:31 +0200

    MIT3 laboratory

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,11 +2,25 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+	public TrainControllerImpl() {
+		Timer t = new Timer();
+		t.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				followSpeed();
+			}
+		}, 1500, 1500);
+
+	}
 
 	@Override
 	public void followSpeed() {
--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -24,6 +24,12 @@ public class TrainSensorImpl implements TrainSensor {
 	public void overrideSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
-	}
 
+		if (speedLimit < 0 || speedLimit > 500 || speedLimit < 0.5 * this.controller.getReferenceSpeed()) {
+			user.setAlarmState(true);
+		}
+		else {
+			user.setAlarmState(false);
+		}
+	}
 }
